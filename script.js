9af170e25a8ece0bb1197cda090ddeb5e2652f06
RocioAldanaMendez <rocioalmendez@gmail.com>
2023-01-22 00:34:06 -0300

    agregando funciones js a pagina pruebas

--- script.js
+++ script.js
@@ -39,3 +39,18 @@ function efectoHabilidades() {
     }
 
 }
+
+
+//pagina pruebas 
+
+
+function confirmacion() {
+	var pregunta = confirm("¿Deseas visitar la página principal?")
+	if (pregunta){
+		alert("Te envío allí rápidamente")
+		window.location = "https://norfipc.com/";
+	}
+	else{
+		alert("Quizás en otro momento...\n Gracias de todas formas")
+	}
+}
