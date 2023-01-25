4a5aa975068d2459f0e00b55a6702e697955dab3
RocioAldanaMendez <rocioalmendez@gmail.com>
2023-01-25 20:48:27 -0300

    se arregló respuesta responsive, se agregó nueva seccion front-back-end, falta darle el movimiento, y se tuvo que eliminar el carrousel

--- script.js
+++ script.js
@@ -63,3 +63,15 @@ function confirmacion() {
 		alert("Quizás en otro momento...\n Gracias de todas formas")
 	}
 }
+
+//movimiento de la sección portfolio
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
