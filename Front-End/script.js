e968ccc592796ca3482cb1d2a2fa1adaec32b917
RocioAldanaMendez <rocioalmendez@gmail.com>
2023-01-27 19:59:51 -0300

    subiendo barra de progreso

--- Front-End/script.js
+++ Front-End/script.js
@@ -1,31 +1,15 @@
-const sliderContainer = document.querySelector('.slider-container')
-const slideRight = document.querySelector('.right-slide')
-const slideLeft = document.querySelector('.left-slide')
-const upButton = document.querySelector('.up-button')
-const downButton = document.querySelector('.down-button')
-const slidesLength = slideRight.querySelectorAll('div').length
+const cargando = document.querySelector('.numero-carga');
+const car = document.querySelector('.carg');
 
-let activeSlideIndex = 0
+let porcentaje = 0;
 
-slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+let total = setInterval( () => {
+    porcentaje+=1
 
-upButton.addEventListener('click', () => changeSlide('up'))
-downButton.addEventListener('click', () => changeSlide('down'))
-
-const changeSlide = (direction) => {
-    const sliderHeight = sliderContainer.clientHeight
-    if(direction === 'up') {
-        activeSlideIndex++
-        if(activeSlideIndex > slidesLength - 1) {
-            activeSlideIndex = 0
-        }
-    } else if(direction === 'down') {
-        activeSlideIndex--
-        if(activeSlideIndex < 0) {
-            activeSlideIndex = slidesLength - 1
-        }
+    if(porcentaje === 100){
+        clearInterval(total)
+        car.textContent=`¡Coming soon ready!!`
     }
 
-    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
-    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
-}
+    cargando.textContent=`${porcentaje}%`
+}, 100);
